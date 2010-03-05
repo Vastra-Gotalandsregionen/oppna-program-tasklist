b169fe1781b2911cc47ff4d089668e7db6a8954e
jonas.liljenfeldt <jonas.liljenfeldt@e75f219c-db35-11de-82ea-1f113a1b7651>
2010-03-05 10:46:59 +0000

    extremely small cleanup /andrew & jonas

--- core-bc/composites/svc/src/test/java/se/vgregion/portal/tasklist/integrationtest/TaskListIntegrationTest.java
+++ core-bc/composites/svc/src/test/java/se/vgregion/portal/tasklist/integrationtest/TaskListIntegrationTest.java
@@ -29,7 +29,6 @@ import java.text.SimpleDateFormat;
 import java.util.Arrays;
 import java.util.List;
 
-import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
