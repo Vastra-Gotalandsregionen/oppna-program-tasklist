934fcdd2970ae3aab7bfc7d4200f7625885ddc91
jonas.liljenfeldt <jonas.liljenfeldt@e75f219c-db35-11de-82ea-1f113a1b7651>
2010-04-08 13:43:19 +0000

    [maven-release-plugin]  copy for tag 1.5

---
+++ tags/1.5/core-bc/composites/svc/src/main/java/se/vgregion/portal/tasklist/domain/Task.java
@@ -0,0 +1,173 @@
+/**
+ * Copyright 2010 Västra Götalandsregionen
+ *
+ *   This library is free software; you can redistribute it and/or modify
+ *   it under the terms of version 2.1 of the GNU Lesser General Public
+ *   License as published by the Free Software Foundation.
+ *
+ *   This library is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU Lesser General Public License for more details.
+ *
+ *   You should have received a copy of the GNU Lesser General Public
+ *   License along with this library; if not, write to the
+ *   Free Software Foundation, Inc., 59 Temple Place, Suite 330,
+ *   Boston, MA 02111-1307  USA
+ *
+ */
+
+package se.vgregion.portal.tasklist.domain;
+
+import java.sql.Date;
+
+import org.apache.commons.lang.builder.EqualsBuilder;
+import org.apache.commons.lang.builder.HashCodeBuilder;
+
+/**
+ * Domain object representing a task.
+ * 
+ * @author jonas
+ * @author david
+ */
+public class Task {
+
+    private long taskId;
+    private String userId = "";
+    private String description = "";
+    private Date dueDate = null;
+    private Priority priority = Priority.LOW;
+    private Status status = Status.OPEN;
+
+    /**
+     * @return the taskId
+     */
+    public long getTaskId() {
+        return taskId;
+    }
+
+    /**
+     * @param taskId
+     *            the taskId to set
+     */
+    public void setTaskId(long taskId) {
+        this.taskId = taskId;
+    }
+
+    /**
+     * @return the description
+     */
+    public String getDescription() {
+        return description;
+    }
+
+    /**
+     * @param description
+     *            the description to set
+     */
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    /**
+     * @return the dueDate
+     */
+    public Date getDueDate() {
+        return dueDate;
+    }
+
+    /**
+     * @param dueDate
+     *            the dueDate to set
+     */
+    public void setDueDate(Date dueDate) {
+        this.dueDate = dueDate;
+    }
+
+    /**
+     * @return the priority
+     */
+    public Priority getPriority() {
+        return priority;
+    }
+
+    /**
+     * @param priority
+     *            the priority to set
+     */
+    public void setPriority(Priority priority) {
+        this.priority = priority;
+    }
+
+    /**
+     * @return the userId
+     */
+    public String getUserId() {
+        return userId;
+    }
+
+    /**
+     * @param userId
+     *            the userId to set
+     */
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    /**
+     * @param status
+     *            the status to set
+     */
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
+    /**
+     * @return the status
+     */
+    public Status getStatus() {
+        return status;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public int hashCode() {
+        HashCodeBuilder builder = new HashCodeBuilder();
+        builder.append(taskId);
+        builder.append(userId);
+        builder.append(description);
+        builder.append(dueDate.getTime());
+        builder.append(priority);
+        builder.append(status);
+        return builder.toHashCode();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        Task taskObj = (Task) obj;
+
+        EqualsBuilder builder = new EqualsBuilder();
+
+        builder.append(taskId, taskObj.taskId);
+        builder.append(userId, taskObj.userId);
+        builder.append(description, taskObj.description);
+        builder.append(dueDate.getTime(), taskObj.dueDate.getTime());
+        builder.append(priority, taskObj.priority);
+        builder.append(status, taskObj.status);
+        return builder.isEquals();
+    }
+}
